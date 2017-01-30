9226e87f3b0e6ac0acc02d89d6f076089eb2234a
Jannik Wortmann <jannikwortmann92@gmail.com>
2017-01-30 21:27:07 +0100

    Controller und Services vorbereitet

---
+++ src/WebApplication1/Controllers/ItemController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    public class ItemController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public Item Get()
+        {
+            Item Item = new Item();
+            Item.ItemId = 1;
+            Item.Title = "Hey";
+            return Item;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
